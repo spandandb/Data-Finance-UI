f5c177eb213f7e76d03bbcae7552fccc73d56598
Spandan Das Barman <spandandb94@gmail.com>
2022-06-24 00:19:31 +0530

    hero section ready

---
+++ src/components/Hero.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import bgVid from '../videos/bg.mp4';
+import Typed from 'react-typed';
+
+export default function Hero() {
+  return (
+    <div className='w-full h-screen top-0'>
+      <video
+        className='object-cover h-screen w-full absolute -z-10 top-0'
+        src={bgVid}
+        autoPlay
+        loop
+        muted
+      />
+      <div className='max-w-[800px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
+        <p className='text-white font-bold p-2 text-lg md:text-2xl'>
+          GROWING WITH DATA ANALYTICS
+        </p>
+        <h1 className='text-green-400 md:text-7xl sm:text-6xl text-4xl font-bold md:py-2'>
+          Grow with data<span className='text-yellow-300'>.</span>
+        </h1>
+        <div className='flex justify-center items-center'>
+          <p className='text-white text-md sm:text-xl md:text-3xl font-semibold py-2'>
+            Fast, flexible financing for{' '}
+          </p>
+          <Typed
+            className='text-yellow-300 font-bold text-md sm:text-xl md:text-3xl ml-2'
+            strings={['BTB', 'BTC', 'SASS']}
+            typeSpeed={160}
+            backSpeed={160}
+            loop
+          />
+        </div>
+        <p className='text-gray-400 font-semibold p-6 text-sm sm:text-md md:text-lg'>
+          Monitor your <span className='text-yellow-300'>Data Analytics</span>{' '}
+          to increase revenue for BTB,BTC & SASS platforms.
+        </p>
+        <button className='bg-green-400 w-fit mx-auto m-10 rounded-md px-8 py-4 text-md md:text-lg font-medium border-2 border-green-400 hover:text-white hover:bg-transparent hover:border-green-400'>
+          Get Started
+        </button>
+      </div>
+    </div>
+  );
+}
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,43 +3,59 @@ import { RiMenu3Fill, RiCloseFill } from 'react-icons/ri';
 
 function Navbar() {
   const [nav, setNav] = useState(false);
+  const [navColor, setNavColor] = useState(false);
 
   const handleNav = () => {
     setNav(!nav);
   };
 
+  const toggleNavBackground = () => {
+    if (window.scrollY >= 50) {
+      setNavColor(true);
+    } else {
+      setNavColor(false);
+    }
+  };
+
+  window.addEventListener('scroll', toggleNavBackground);
+
   return (
-    <div className='text-white flex justify-between items-center h-24 mx-auto max-w-[1240px] px-4'>
-      <h1 className='text-3xl font-bold text-green-400'>Data.</h1>
-      <ul className='hidden md:flex'>
-        <li className='p-4'>Home</li>
-        <li className='p-4'>Company</li>
-        <li className='p-4'>Resources</li>
-        <li className='p-4'>About</li>
-        <li className='p-4'>Contact</li>
-      </ul>
-      <div onClick={handleNav} className='md:hidden'>
-        {!nav ? (
-          <RiMenu3Fill size={20} className='ease-in-out duration-500' />
-        ) : (
-          <RiCloseFill size={20} className='ease-in-out duration-500' />
-        )}
-      </div>
-      <div
-        className={
-          nav
-            ? 'fixed left-0 top-0 w-2/3 border-r-gray-700 h-full border-r bg-[#000300] ease-in-out duration-1000 md:hidden'
-            : 'fixed hidden'
-        }
-      >
-        <h1 className='text-3xl font-semibold text-green-400 m-4'>Menu</h1>
-        <ul className='pt-5 uppercase'>
-          <li className='p-4 border-b border-gray-500'>Home</li>
-          <li className='p-4 border-b border-gray-500'>Company</li>
-          <li className='p-4 border-b border-gray-500'>Resources</li>
-          <li className='p-4 border-b border-gray-500'>About</li>
+    <div className={`w-full top-0 sticky ${navColor ? 'bg-black/60' : ''}`}>
+      <div className='text-white flex justify-between items-center h-20 mx-auto max-w-[1240px] px-4'>
+        <h1 className='text-3xl font-bold text-green-400'>
+          Data<span className='text-white'>Fin</span>
+          <span className='text-yellow-300'>.</span>
+        </h1>
+        <ul className='hidden md:flex'>
+          <li className='p-4'>Home</li>
+          <li className='p-4'>Company</li>
+          <li className='p-4'>Resources</li>
+          <li className='p-4'>About</li>
           <li className='p-4'>Contact</li>
         </ul>
+        <div onClick={handleNav} className='md:hidden'>
+          {!nav ? (
+            <RiMenu3Fill size={20} className='ease-in-out duration-500' />
+          ) : (
+            <RiCloseFill size={20} className='ease-in-out duration-500' />
+          )}
+        </div>
+        <div
+          className={
+            nav
+              ? 'fixed left-0 top-0 w-2/3 border-r-gray-700 h-full border-r bg-[#000300] ease-in-out duration-1000 md:hidden'
+              : 'fixed hidden'
+          }
+        >
+          <h1 className='text-3xl font-semibold text-green-400 m-4'>Menu</h1>
+          <ul className='pt-5 uppercase'>
+            <li className='p-4 border-b border-gray-500'>Home</li>
+            <li className='p-4 border-b border-gray-500'>Company</li>
+            <li className='p-4 border-b border-gray-500'>Resources</li>
+            <li className='p-4 border-b border-gray-500'>About</li>
+            <li className='p-4'>Contact</li>
+          </ul>
+        </div>
       </div>
     </div>
   );
